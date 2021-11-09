bf2e779524c3dd4f6d449f9400dfe097f1f8ae69
lifeng <lifeng980611@gmail.com>
2021-11-09 18:03:54 +0800

    first commit

---
+++ src/components/navbar/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <b-container fluid>
+        <b-row>
+            <b-col cols="3">111</b-col>
+            <b-col cols="3">111</b-col>
+            <b-col cols="3">111</b-col>
+            <b-col cols="3">111</b-col>
+            <b-col cols="3">111</b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="less">
+
+</style>
